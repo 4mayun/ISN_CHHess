0147c9a71bafeef2790c906374212586adfbccb8
4mayun <amayun.houery@e.rascol.net>
2017-12-15 17:45:04 +0100

    Modification de la manière dont l'objet config est initialisé

--- js/chhess_main.js
+++ js/chhess_main.js
@@ -1,8 +1,8 @@
 var game = new Chess()
 
 function setup() {
-  createCanvas(game.config.size.w, game.config.size.h);
-  background(game.config.backgroundColor)
+  createCanvas(game.config.sizeW, game.config.sizeW);
+  background(game.config.backgroundColor);
 }
 
 function draw() {
