3ef591d23c74bf516106df5922b595951fa6715c
Amayun <amayun.houery@e.rascol.net>
2017-12-17 17:13:18 +0100

    New cursor, overing chessboard make it lighter + can resize the window

--- js/chhess_main.js
+++ js/chhess_main.js
@@ -1,9 +1,20 @@
 var game = new Chess()
 
 function setup() {
+  createCanvas(windowWidth-20, windowHeight-20);
   game.init();
 }
 
 function draw() {
+  background(game.config.backgroundColor);
   game.draw();
+}
+
+function mouseMoved() {
+  game.setOverCasing(mouseX, mouseY);
+}
+
+function windowResized() {
+  createCanvas(windowWidth-20, windowHeight-20);
+  background(game.config.backgroundColor);
 }
