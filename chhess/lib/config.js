85d192534c5f1ca98736c48890fe92b68ff87117
Amayun <amayun.houery@e.rascol.net>
2018-01-16 22:16:20 +0100

    Ajout des images des pieces et de leur chemin dans l'objet config

--- chhess/lib/config.js
+++ chhess/lib/config.js
@@ -10,9 +10,37 @@ class Config {
 
     this.maxFrameRate = 60;
 
-    this.cursor = { // Chemin vers les fichiers d'icônes
-      dark: "ressources/cursor/dark_matter.png",
-      hot: "ressources/cursor/hot_matter.png"
+    this.ressource = { // Chemins vers les fichiers ressources
+      piece: {
+        king: {
+          white: "chhess/ressource/pieces/roi_blanc.png",
+          black: "chhess/ressource/pieces/roi_noir.png"
+        },
+        queen: {
+          white: "chhess/ressource/pieces/reine_blanche.png",
+          black: "chhess/ressource/pieces/reine_noire.png"
+        },
+        bishop: {
+          white: "chhess/ressource/pieces/fou_blanc.png",
+          black: "chhess/ressource/pieces/fou_noir.png"
+        },
+        knight: {
+          white: "chhess/ressource/pieces/cheval_blanc.png",
+          black: "chhess/ressource/pieces/cheval_noir.png"
+        },
+        rook: {
+          white: "chhess/ressource/pieces/tour_blanche.png",
+          black: "chhess/ressource/pieces/tour_noire.png"
+        },
+        pawn: {
+          white: "chhess/ressource/pieces/pion_blanc.png",
+          black: "chhess/ressource/pieces/pion_noir.png"
+        }
+      },
+      cursor: { // Chemin vers les fichiers d'icônes
+        dark: "chhess/ressources/cursor/dark_matter.png",
+        hot: "chhess/ressources/cursor/hot_matter.png"
+      }
     };
   }
 }
